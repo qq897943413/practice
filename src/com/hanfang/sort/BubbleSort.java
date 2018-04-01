41a5d12a326fec68c8ddaf8baac78113aadb93c6
qq897943413 <897943413@qq.com>
2018-04-01 16:47:38 +0800

    No.2

--- src/com/hanfang/sort/BubbleSort.java
+++ src/com/hanfang/sort/BubbleSort.java
@@ -1,7 +1,27 @@
 package com.hanfang.sort;
-
+//冒泡排序
+//第一个和第二个比  第二个和第三个比  直到最后一个最大 以此类推
 public class BubbleSort {
-    public static void bubbleSort(int[] array){
-        int length=array.length;
+    private static void bubbleSort(int[] array) {
+        int length = array.length;
+        int temp;
+        for (int out = length; out > 0; out--) {
+            for (int in = 0; in < out - 1; in++) {
+                if (array[in] > array[in + 1]) {
+                    temp = array[in];
+                    array[in] = array[in + 1];
+                    array[in + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] array={3,6,4,4,9,2,1,7,5,8};
+        bubbleSort(array);
+        for(int i:array){
+            System.out.println(i);
+        }
     }
 }
