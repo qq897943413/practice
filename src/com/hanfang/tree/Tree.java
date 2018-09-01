dc6aad6fca02323881fc70d979afd49854a61745
qq897943413 <897943413@qq.com>
2018-09-01 17:55:10 +0800

    finished

---
+++ src/com/hanfang/tree/Tree.java
@@ -0,0 +1,8 @@
+package com.hanfang.tree;
+
+public class Tree {
+    private Node root;
+    public void find(int key){}
+    public void delete(int key){}
+    private void insert(int key,double dd){}
+}
