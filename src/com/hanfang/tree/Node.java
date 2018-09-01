a14d1d78901625a796f4dccf7a1ba86c9794dee6
qq897943413 <897943413@qq.com>
2018-09-01 13:50:20 +0800

    finished

---
+++ src/com/hanfang/tree/Node.java
@@ -0,0 +1,11 @@
+package com.hanfang.tree;
+
+public class Node {
+    int iDate;
+    double fDate;
+    Node leftChild;
+    Node rightChild;
+    public void displayNode(){
+        System.out.println(iDate);
+    }
+}
