fcba40f403182807256a6f1e6d20c0fdeb120828
qq897943413 <897943413@qq.com>
2018-12-31 16:58:23 +0800

    finished

---
+++ src/com/hanfang/javaThink/Fuck.java
@@ -0,0 +1,9 @@
+package com.hanfang.javaThink;
+
+public class Fuck {
+    private static String name="汪倩";
+
+    public static void main(String[] args) {
+        System.out.println("Fuck  "+name);
+    }
+}
