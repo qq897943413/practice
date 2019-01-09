4678c337293be7dd61a544fb648dfd04f0db1cde
qq897943413 <30482169+qq897943413@users.noreply.github.com>
2019-01-09 21:13:02 +0800

    Update FactoryTest.java

--- src/com/hanfang/designpattern/commonfactorypattern/FactoryTest.java
+++ src/com/hanfang/designpattern/commonfactorypattern/FactoryTest.java
@@ -3,7 +3,7 @@ package com.hanfang.designpattern.commonfactorypattern;
 public class FactoryTest {
     public static void main(String[] args) {
         SendFactory sf = new SendFactory();
-        Sender send = sf.send("sms");
+        Sender send = sf.send("mail");
         send.send();
 
     }
